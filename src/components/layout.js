218abd47813bf0bef3608334bb26df1bd5b650e6
Matt Malone <mattagape@me.com>
2021-08-18 14:24:16 +0700

    Set up header

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import Helmet from "react-helmet";
 
-import Header from "./header2"
+import Header from "./header"
 import Footer from "./footer";
 import "./layout.css"
 import "../css/style.css";
