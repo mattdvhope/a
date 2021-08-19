30e0f8ae25ae2bad49e13485115c9a1bb037e5eb
Matt Malone <mattagape@me.com>
2021-08-19 19:57:24 +0700

    Extracted code to SetFirstVideoPosition

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -3,37 +3,12 @@ import { Link, StaticQuery, graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import YoutubeHolder from "./YoutubeHolder"
+import SetFirstVideoPosition from "../utils/SetFirstVideoPosition";
 
 const BlogPost = ({ data }) => {
 
-  // To set the first video's position AFTER the DOM loaded...////
   const firstVideoRef = useRef(null)
-  const [firstVideoPosition, setFirstVideoPosition] = useState(null);
-
-  const [rerender, setRerender] = useState(); // or any state
-  const [afterRender, setAfterRender] = useState();// internal state
-
-  useEffect(() => {
-    if (!afterRender) return;
-    setFirstVideoPosition(firstVideoRef.current.getBoundingClientRect().top);
-    setAfterRender(false);
-  }, [afterRender]);
-
-  useEffect(() => {
-    setAfterRender(true); // (1) will be called after DOM rendered
-    console.log(firstVideoPosition)
-  }, [rerender]); // or don't set any if you want to listen to all re-render events
-
-  if (firstVideoPosition !== null) {
-    setTimeout(function(){
-      window.scrollTo({
-        top: firstVideoPosition,
-        behavior: "smooth"
-      })
-    }, 1700);
-  };
-  /////////////////////////////////////////////////////////////////
-
+  SetFirstVideoPosition(firstVideoRef)
 
   // Videos above and below the first video (which is arrived at via the URL)
   const slug = data.contentfulBlogs.slug;
---
+++ src/utils/SetFirstVideoPosition.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useRef } from "react";
+
+  // To set the first video's position AFTER the DOM loaded...////
+const SetFirstVideoPosition = (firstVideoRef) => {
+
+  const [firstVideoPosition, setFirstVideoPosition] = useState(null);
+
+  const [rerender, setRerender] = useState(); // or any state
+  const [afterRender, setAfterRender] = useState();// internal state
+
+  useEffect(() => {
+    if (!afterRender) return;
+    setFirstVideoPosition(firstVideoRef.current.getBoundingClientRect().top);
+    setAfterRender(false);
+  }, [afterRender]);
+
+  useEffect(() => {
+    setAfterRender(true); // (1) will be called after DOM rendered
+    console.log(firstVideoPosition)
+  }, [rerender]); // or don't set any if you want to listen to all re-render events
+
+  if (firstVideoPosition !== null) {
+    setTimeout(function(){
+      window.scrollTo({
+        top: firstVideoPosition,
+        behavior: "smooth"
+      })
+    }, 1700);
+  };
+
+};
+
+export default SetFirstVideoPosition;
+
+
+
