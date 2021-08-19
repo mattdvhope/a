4ac6b28168bb62cd8a27f09f8b7878b05fee4ea5
Matt Malone <mattagape@me.com>
2021-08-19 19:44:01 +0700

    Worked on scrolling

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -1,30 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
 import { Link, StaticQuery, graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import YoutubeHolder from "./YoutubeHolder"
 
 const BlogPost = ({ data }) => {
+
+  // To set the first video's position AFTER the DOM loaded...////
+  const firstVideoRef = useRef(null)
+  const [firstVideoPosition, setFirstVideoPosition] = useState(null);
+
+  const [rerender, setRerender] = useState(); // or any state
+  const [afterRender, setAfterRender] = useState();// internal state
+
+  useEffect(() => {
+    if (!afterRender) return;
+    setFirstVideoPosition(firstVideoRef.current.getBoundingClientRect().top);
+    setAfterRender(false);
+  }, [afterRender]);
+
+  useEffect(() => {
+    setAfterRender(true); // (1) will be called after DOM rendered
+    console.log(firstVideoPosition)
+  }, [rerender]); // or don't set any if you want to listen to all re-render events
+
+  if (firstVideoPosition !== null) {
+    setTimeout(function(){
+      window.scrollTo({
+        top: firstVideoPosition,
+        behavior: "smooth"
+      })
+    }, 1700);
+  };
+  /////////////////////////////////////////////////////////////////
+
+
+  // Videos above and below the first video (which is arrived at via the URL)
+  const slug = data.contentfulBlogs.slug;
+  const [evenMoreVideos, setEvenMoreVideos] = useState(null);
+  const [additionalVideos, setAdditionalVideos] = useState(null);
+
+  useLayoutEffect(() => {
+    setAdditionalVideos(() => 
+      <div className="site-container blog-post">
+        {data.allContentfulBlogs.edges
+          .filter(edge => edge.node.slug === "i-am")
+          .map(({ node }, i) => (
+            <div key={i} className="container">
+              <YoutubeHolder data={node} />
+              <br/>
+              <hr/>
+              <hr/>
+            </div>
+          )
+        )}
+      </div>
+    )
+  }, []);
+
+  useLayoutEffect(() => {
+    setEvenMoreVideos(() =>
+      <div className="site-container blog-post">
+        {data.allContentfulBlogs.edges
+          .filter(edge => edge.node.slug === "doubting-thomas")
+          .map(({ node }, i) => (
+            <div key={i} className="container">
+              <YoutubeHolder data={node} />
+              <br/>
+              <hr/>
+              <hr/>
+            </div>
+          )
+        )}
+      </div>
+    )
+  }, []);
+
   return (
     <Layout>
-      <div className="container">
+      {evenMoreVideos}
+      <hr/>
+      <div className="container" ref={firstVideoRef}>
         <div className="site-container blog-post">
           <YoutubeHolder data={data.contentfulBlogs}/>
         </div>
       </div>
       <hr/>
-
-      <div className="site-container blog-post" style={{ marginTop: `-6em`}} >
-        {data.allContentfulBlogs.edges.map(({ node }, i) => (
-          <div key={i} className="container">
-            <YoutubeHolder data={node} />
-            <br/>
-            <hr/>
-            <hr/>
-          </div>
-        ))}
-      </div>
-
+      {additionalVideos}
     </Layout>
   )
 }
@@ -90,19 +152,3 @@ export const pageQuery = graphql`
 
   }
 `;
-
-
-
-  // return (
-  //   <div className="site-container blog-post">
-  //     {edges.map(({ node }, i) => (
-  //       <div key={i} className="container">
-  //         <YoutubeHolder data={node} />
-  //         <br/>
-  //         <hr/>
-  //         <hr/>
-  //         <br/>
-  //       </div>
-  //     ))}
-  //   </div>
-  // )
