7a9b5ab6778746120eb09f608c819102e3242037
Matt Malone <mattagape@me.com>
2021-08-23 17:16:18 +0700

    Set up scrolling

--- src/utils/RetainInitVidBlwPos.js
+++ src/utils/RetainInitVidBlwPos.js
@@ -14,20 +14,19 @@ const RetainInitVidBlwPos = (initVidBlwRef) => {
   // Replacement for the now unusable JS method, 'ScrollIntoView()'
   const ScrollElementIntoView = (element, behavior) => {
 
-
-    let scrollTop = window.pageYOffset || element.scrollTop
+    // let scrollTop = window.pageYOffset || element.scrollTop
+    let scrollTop = element.scrollTop
 
     // Furthermore, if you have for example a 'container' outside the element 
     // you need to factor in its dimensions when calculating the position to scroll to
     const headerOutsideIframe = window.parent.document.getElementsByClassName('container')[0].clientHeight
 
     const finalOffset = element.getBoundingClientRect().top + scrollTop + headerOutsideIframe;
-    // const finalOffset = headerOutsideIframe;
 
 console.log(element.getBoundingClientRect().top)
 console.log(scrollTop)
 console.log(headerOutsideIframe)
-
+console.log(document.documentElement.offsetHeight)
 
     window.parent.scrollTo({
       top: finalOffset,
