5dfb83209d9972e61c6b2e2328399f2074ec67f8
Matt Malone <mattagape@me.com>
2021-08-23 15:09:48 +0700

    Set up scrolling

--- src/utils/RetainInitVidBlwPos.js
+++ src/utils/RetainInitVidBlwPos.js
@@ -1,4 +1,4 @@
-// import { ElementAppendedViaScrolling } from "../utils/ElementAppendedViaScrolling";
+// import { ScrollElementIntoView } from "../utils/ScrollElementIntoView";
 
 const RetainInitVidBlwPos = (initVidBlwRef) => {
 
@@ -8,17 +8,17 @@ const RetainInitVidBlwPos = (initVidBlwRef) => {
 
   myPromise
   .then(res => initVidBlwRef.current)
-  .then(res => ElementAppendedViaScrolling(res, 'instant'))
+  .then(res => ScrollElementIntoView(res, 'instant'))
   .catch(err => console.log("error: ", err));
 
   // Replacement for the now unusable JS method, 'ScrollIntoView()'
-  const ElementAppendedViaScrolling = (element, behavior) => {
+  const ScrollElementIntoView = (element, behavior) => {
 
     let scrollTop = window.pageYOffset || element.scrollTop
 
-     // Furthermore, if you have for example a 'container' outside the element 
-     // you need to factor in its dimensions when calculating the position to scroll to
-     const headerOutsideIframe = window.parent.document.getElementsByClassName('container')[0].clientHeight
+    // Furthermore, if you have for example a 'container' outside the element 
+    // you need to factor in its dimensions when calculating the position to scroll to
+    const headerOutsideIframe = window.parent.document.getElementsByClassName('container')[0].clientHeight
 
     // const finalOffset = element.getBoundingClientRect().top + scrollTop + headerOutsideIframe - 150;
     const finalOffset = document.documentElement.scrollTop;
