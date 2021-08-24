b848cca202f5d7fb262c7d605ef06f9a401fca4b
Matt Malone <mattagape@me.com>
2021-08-24 12:16:50 +0700

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,13 +39,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Matt Malone`,
+        short_name: `Matt`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/fev-birds.png`, // This is the favicon.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
