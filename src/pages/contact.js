3bbf2c14e91b4d365935f48f0e863cf866e89592
Matt Malone <mattagape@me.com>
2021-08-31 16:47:13 +0700

    Setting up cookie

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
-import * as React from "react"
+import React, { useEffect } from "react";
+
 import axios from 'axios';
 
 import Cookies from 'js-cookie' // REMOVE THIS PACKAGE SOON!!!!!!!
@@ -8,54 +9,42 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 
-const instance = axios.create({
-  baseURL: 'https://e2doix.herokuapp.com/feed/',
-  timeout: 1000,
-  headers: {'X-CSRFToken': csrftoken} // 'X-CSRFToken': csrftoken
-});
+const Contact = () => {
 
-// const csrftoken = getCookie('csrftoken');
-const csrftoken = Cookies.get('csrftoken');
+  useEffect(() => {
+    const instance = axios.create({
+      baseURL: 'https://e2doix.herokuapp.com/feed/',
+      timeout: 1000,
+      headers: {'X-CSRFToken': csrftoken} // 'X-CSRFToken': csrftoken
+    });
 
-console.log(csrftoken);
+    // const csrftoken = getCookie('csrftoken');
+    const csrftoken = Cookies.get('csrftoken');
 
-// function getCookie(name) {
-//   let cookieValue = null;
-//   if (document.cookie && document.cookie !== '') {
-//     const cookies = document.cookie.split(';');
-//     for (let i = 0; i < cookies.length; i++) {
-//       const cookie = cookies[i].trim();
-//       // Does this cookie string begin with the name we want?
-//       if (cookie.substring(0, name.length + 1) === (name + '=')) {
-//         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-//         break;
-//       }
-//     }
-//   }
-//   return cookieValue;
-// }
+    console.log(csrftoken);
+  }, []);
 
+  return (
+    <Layout header="Contact Us">
+      <Seo title="Contact us" />
+      <br/>
+      <br/>
+      <br/>
+      <h1>Contact Us!</h1>
+      <br/>
+      <br/>
+      <h1>
+        <button
 
-const Contact = () => (
-  <Layout header="Contact Us">
-    <Seo title="Contact us" />
-    <br/>
-    <br/>
-    <br/>
-    <h1>Contact Us!</h1>
-    <br/>
-    <br/>
-    <h1>
-      <button
-
-        onClick={() => {
-          // invoke functions here
-        }}
-      >Click here to go to Django</button>
-    </h1>
-    <br/>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+          onClick={() => {
+            // invoke functions here
+          }}
+        >Click here to go to Django</button>
+      </h1>
+      <br/>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default Contact
