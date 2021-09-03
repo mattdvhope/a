ab89911b8799a75bb4bf45b9fb3fdf2342f5707c
Matt Malone <mattagape@me.com>
2021-09-03 11:50:04 +0700

    Experimenting w/ scrolling

--- src/utils/RetainPosOrJumpToTop.js
+++ src/utils/RetainPosOrJumpToTop.js
@@ -8,17 +8,23 @@ const RetainPosOrJumpToTop = (initVidBlwRef, numberOfVideosBelow) => {
   .catch(err => console.log("error: ", err));
 
   const ScrollElementIntoView = (element, behavior) => {
-    const scrollTop = document.documentElement.scrollTop;
+    const heightAboveWindowScreen = document.documentElement.scrollTop;
+    const windowScreenHeight = window.innerHeight
+    const heightOfDocument = document.documentElement.offsetHeight;
 
-    // Jump to top...
-    if (numberOfVideosBelow === 1 || numberOfVideosBelow === 0 || window.innerHeight + scrollTop >= document.documentElement.offsetHeight) {
+    // Jump to top after scrolling below either of the last two (firstVid) videos OR when the full feed hits bottom...
+    if (numberOfVideosBelow === 1 || numberOfVideosBelow === 0 || heightAboveWindowScreen + windowScreenHeight >= heightOfDocument) {
       window.scrollTo({
         top: 0,
         behavior: 'instant'
       })
-    } else { // Retain Position
+    } else { // Retain window screen Position when 'more' above/below videos are appended
       const containerElementHeight = window.parent.document.getElementsByClassName('container')[0].clientHeight
-      const finalOffset = element.getBoundingClientRect().top + scrollTop + containerElementHeight - element.offsetHeight;
+
+      console.log(heightOfDocument)
+      console.log(containerElementHeight)
+
+      const finalOffset = element.getBoundingClientRect().top + heightAboveWindowScreen + containerElementHeight - element.offsetHeight;
       window.parent.scrollTo({
         top: finalOffset,
         behavior: behavior || 'instant'
