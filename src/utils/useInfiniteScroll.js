3dd386dfb8484bbaa293bd7a6d2db95aa88a784a
Matt Malone <mattagape@me.com>
2021-09-06 17:21:59 +0700

    Set up range for integer values of doc height

--- src/utils/useInfiniteScroll.js
+++ src/utils/useInfiniteScroll.js
@@ -7,8 +7,14 @@ const useInfiniteScroll = (elementsFromScrolling) => {
   // Add elements above & below when window Y-ends reached
   useEffect(() => {
     const handleScroll = () => {
-      const scrollTop = document.documentElement.scrollTop;
-      if (window.innerHeight + scrollTop !== document.documentElement.offsetHeight) {
+      const winHt = Math.ceil(window.innerHeight);
+      const scrollTop = Math.ceil(document.documentElement.scrollTop);
+      const docHt = Math.ceil(document.documentElement.offsetHeight);
+
+      // console.log(Array(7).fill(winHt + scrollTop).map((x, y) => x + y).includes(docHt))
+
+      // if (winHt + scrollTop !== docHt) {
+      if (!(Array(7).fill(winHt + scrollTop).map((x, y) => x + y).includes(docHt))) {
         return;
       } 
       setIsFetching(true);
