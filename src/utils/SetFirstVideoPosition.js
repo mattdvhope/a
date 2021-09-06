3bd9f551fdb745807d28b9df4bd84bf708a84c4b
Matt Malone <mattagape@me.com>
2021-09-06 18:48:16 +0700

    Added browser detect for scroll speed control

--- src/utils/SetFirstVideoPosition.js
+++ src/utils/SetFirstVideoPosition.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
+import { detect } from "detect-browser";
+
 import { ScrollToSmoothly } from "./ScrollToSmoothly"
 
 // To set the first video's position AFTER the DOM loaded...////
@@ -12,9 +14,12 @@ const SetFirstVideoPosition = (firstVideoRef, slug) => {
       resolve('foo');
     });
 
+    const browser = detect();
+    const scrollSpeed = browser.name === "chromium-webview" ? 3000 : 1400;
+
     myPromise
     .then(res => setFirstVideoPosition(firstVideoRef.current.getBoundingClientRect().top))
-    .then(res => ScrollToSmoothly(firstVideoPosition, 1400))
+    .then(res => ScrollToSmoothly(firstVideoPosition, scrollSpeed))
     .catch(err => console.log("error: ", err));
 
     window.sessionStorage.setItem('slug', slug);
