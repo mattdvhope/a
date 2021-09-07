1a817065eabc8411579cda3ba38bd020b67f76ce
Matt Malone <mattagape@me.com>
2021-09-07 07:39:28 +0700

    Back to using local function, ScrollToSmoothly

--- src/utils/SetFirstVideoPosition.js
+++ src/utils/SetFirstVideoPosition.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { detect } from "detect-browser";
-
-import { ScrollToSmoothly } from "./ScrollToSmoothly"
+import { ScrollToSmoothly } from "./ScrollToSmoothly";
 
 // To set the first video's position AFTER the DOM loaded...////
 const SetFirstVideoPosition = (firstVideoRef, slug) => {
@@ -15,18 +14,14 @@ const SetFirstVideoPosition = (firstVideoRef, slug) => {
     });
 
     const browser = detect();
+    const scrollSpeed = browser.name === "chromium-webview" ? 2400 : 1400;
 
     myPromise
     .then(res => setFirstVideoPosition(firstVideoRef.current.getBoundingClientRect().top))
-    .then(res => {
-      if (browser.name === "chromium-webview") {
-        firstVideoRef.current.scrollIntoView({behavior: "smooth"});
-      } else {
-        ScrollToSmoothly(firstVideoPosition, 1400);
-      }
-    })
+    .then(res => ScrollToSmoothly(firstVideoPosition, scrollSpeed))
     .catch(err => console.log("error: ", err));
 
+    // To set default video post for the session
     window.sessionStorage.setItem('slug', slug);
 
   }, [firstVideoPosition]);
