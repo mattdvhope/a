982b140a7d37322583ffd05b9d95087ffad931c0
Matt Malone <mattagape@me.com>
2021-09-07 11:06:05 +0700

    Changed back range to 7

--- src/utils/InRangeOfDocHeight.js
+++ src/utils/InRangeOfDocHeight.js
@@ -1,7 +1,7 @@
 const InRangeOfDocHeight = (scrollTop, winHt, docHt) => {
 
   // Is winHt + scrollTop w/in range (+3 or -3) of docHt...?
-	return Array(11).fill(scrollTop + winHt).map((x, y) => x + y).includes(docHt);
+	return Array(7).fill(scrollTop + winHt).map((x, y) => x + y).includes(docHt);
 
 }
 
