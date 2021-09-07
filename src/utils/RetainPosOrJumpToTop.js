dada80b0d8c76126acfe01da367eb764700b1aad
Matt Malone <mattagape@me.com>
2021-09-07 09:31:55 +0700

    Moved initVidBlwHeight into else

--- src/utils/RetainPosOrJumpToTop.js
+++ src/utils/RetainPosOrJumpToTop.js
@@ -3,15 +3,15 @@ import ReachedButtomOfDoc from './ReachedButtomOfDoc'
 const RetainPosOrJumpToTop = (initVidBlwRef, numberOfVideosBelow) => {
 
   const myPromise = new Promise((resolve, reject) => {
-    resolve(initVidBlwRef.current);
+    resolve();
   });
+
   myPromise
-  .then(res => ScrollElementIntoView(res, 'instant'))
+  .then(res => ScrollElementIntoView())
   .catch(err => console.log("error: ", err));
 
-  const ScrollElementIntoView = (element, behavior) => {
+  const ScrollElementIntoView = () => {
     const windowScreenHeight = window.innerHeight;
-    const initVidBlwHeight = initVidBlwRef.current.offsetHeight;
 
     // Jump to top of document when bottom reached
     if (numberOfVideosBelow === 1 || numberOfVideosBelow === 0 || ReachedButtomOfDoc()) {
@@ -20,11 +20,12 @@ const RetainPosOrJumpToTop = (initVidBlwRef, numberOfVideosBelow) => {
         behavior: 'instant'
       })
     } else { // Retain window screen Position when 'more' above/below videos are appended
+      const initVidBlwHeight = initVidBlwRef.current.offsetHeight;
       const diff = windowScreenHeight - initVidBlwHeight;
       const gap = diff >= 0 ? diff : 0;
       window.parent.scrollTo({
         top: initVidBlwRef.current.offsetTop - gap,
-        behavior: behavior || 'instant'
+        behavior: 'instant'
       })
     }
   } // ScrollElementIntoView
