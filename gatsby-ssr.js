bbf3e066cfed22b5f37e84e4e053b7ceae6d5ba0
Matt Malone <mattagape@me.com>
2021-09-14 11:22:57 +0700

    Testing methods

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,34 +1,34 @@
-import React from "react"
-// This is for the website FB Chat Messenger (on bottom-right of website)
-export const onRenderBody = (
-  { setHeadComponents },
-  pluginOptions
-) => {
-  setHeadComponents([
-    <script
-      key="fun_javascript"
-      dangerouslySetInnerHTML={{ __html: `
-        window.fbAsyncInit = function() {
-          window.FB.init({
-            xfbml            : ${true},
-            version          : 'v11.0'
-          });
-        };
+// import React from "react"
+// // This is for the website FB Chat Messenger (on bottom-right of website)
+// export const onRenderBody = (
+//   { setHeadComponents },
+//   pluginOptions
+// ) => {
+//   setHeadComponents([
+//     <script
+//       key="fun_javascript"
+//       dangerouslySetInnerHTML={{ __html: `
+//         window.fbAsyncInit = function() {
+//           window.FB.init({
+//             xfbml            : ${true},
+//             version          : 'v11.0'
+//           });
+//         };
 
-console.log("inside code");
-        (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-      `,
-      }}
-    />,
-    <div class="fb-customerchat"
-      attribution="page_inbox"
-      page_id="106619367753772">
-    </div>
-  ])
-}
+// console.log("inside code");
+//         (function(d, s, id) {
+//           var js, fjs = d.getElementsByTagName(s)[0];
+//           if (d.getElementById(id)) return;
+//           js = d.createElement(s); js.id = id;
+//           js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+//           fjs.parentNode.insertBefore(js, fjs);
+//         }(document, 'script', 'facebook-jssdk'));
+//       `,
+//       }}
+//     />,
+//     <div class="fb-customerchat"
+//       attribution="page_inbox"
+//       page_id="106619367753772">
+//     </div>
+//   ])
+// }
