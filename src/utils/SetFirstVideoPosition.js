5ccadc6ed483e3e4cb6acfc7d223781e8e44a096
Matt Malone <mattagape@me.com>
2021-09-14 22:39:52 +0700

    Check methods

--- src/utils/SetFirstVideoPosition.js
+++ src/utils/SetFirstVideoPosition.js
@@ -9,21 +9,21 @@ const SetFirstVideoPosition = (firstVideoRef, slug) => {
 
   useEffect(() => {
 
-    const browser = detect();
-    const node = /node/i;
-    const fb = /facebook/i;
+    // const browser = detect();
+    // const node = /node/i;
+    // const fb = /facebook/i;
 
-    if (node.test(browser.name.toString()) || fb.test(browser.name.toString())) {
+    // if (node.test(browser.name.toString()) || fb.test(browser.name.toString())) {
       ScrollToSmoothly(500, 1000);
-    } else {
-      const myPromise = new Promise((resolve, reject) => {
-        resolve(setFirstVideoPosition(firstVideoRef.current.getBoundingClientRect().top));
-      });
-      const scrollSpeed = browser.name === "chromium-webview" ? 1800 : 1200;
-      myPromise
-      .then(res => firstVideoPosition || firstVideoPosition === 0 ? ScrollToSmoothly(firstVideoPosition, scrollSpeed) : null)
-      .catch(err => console.log("error: ", err));
-    } // if-else
+    // } else {
+    //   const myPromise = new Promise((resolve, reject) => {
+    //     resolve(setFirstVideoPosition(firstVideoRef.current.getBoundingClientRect().top));
+    //   });
+    //   const scrollSpeed = browser.name === "chromium-webview" ? 1800 : 1200;
+    //   myPromise
+    //   .then(res => firstVideoPosition || firstVideoPosition === 0 ? ScrollToSmoothly(firstVideoPosition, scrollSpeed) : null)
+    //   .catch(err => console.log("error: ", err));
+    // } // if-else
 
  
     // To set default video post for the session
