5c1d3286fc23608dd51d20526a744152aabd970c
Matt Malone <mattagape@me.com>
2021-09-14 18:05:59 +0700

    Checking methods

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,20 +3,10 @@ import PageTemplate from "../components/PageTemplate"
 import Layout from "../components/layout" // REMOVE THIS SOON!!!!!!!
 
 const About = () => 
-    <Layout>;
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <h1>Hello from ABOUT!!!!!</h1>
-    </Layout>;
-
-
-
-  // <PageTemplate
-  //   header="เกี่ยวกับเรา"
-  //   title="เกี่ยวกับเรา"
-  //   pageType="about"
-  // />
+  <PageTemplate
+    header="เกี่ยวกับเรา"
+    title="เกี่ยวกับเรา"
+    pageType="about"
+  />
 
 export default About
