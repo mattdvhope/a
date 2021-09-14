71d3c4b7097f9e7ee34c6dd8c13922324c40fbbf
Matt Malone <mattagape@me.com>
2021-09-14 17:05:45 +0700

    Removed Layout wrapper

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,37 +9,35 @@ import PageTemplate from "../components/PageTemplate"
 
 const Contact = () => {
 
-  useEffect(() => { // This for Luke's backend!!!!!
-    const csrftoken = Cookies.get('csrftoken');
+  // useEffect(() => { // This for Luke's backend!!!!!
+  //   const csrftoken = Cookies.get('csrftoken');
 
-    let _data = {
-      userid: "user-7j2kgE5H8LGYpRdT8KmGbtUSC"
-    }
+  //   let _data = {
+  //     userid: "user-7j2kgE5H8LGYpRdT8KmGbtUSC"
+  //   }
 
-    const request = new Request(
-      'https://e2doix.herokuapp.com/feed' //,
-      // {headers: {'X-CSRFToken': csrftoken}}
-    );
-    fetch(request, {
-      method: 'POST',
-      body: JSON.stringify(_data)
-    }).then(function(response) {
-      console.log(response);
-      console.log(csrftoken);
-    }).catch(function(error) {
-      console.log(error);
-    });
+  //   const request = new Request(
+  //     'https://e2doix.herokuapp.com/feed' //,
+  //     // {headers: {'X-CSRFToken': csrftoken}}
+  //   );
+  //   fetch(request, {
+  //     method: 'POST',
+  //     body: JSON.stringify(_data)
+  //   }).then(function(response) {
+  //     console.log(response);
+  //     console.log(csrftoken);
+  //   }).catch(function(error) {
+  //     console.log(error);
+  //   });
 
-  }, []);
+  // }, []);
 
   return (
-    <Layout>
-      <PageTemplate
-        header="ติดต่อเรา"
-        title="ติดต่อเรา"
-        pageType="contact"
-      />
-    </Layout>
+    <PageTemplate
+      header="ติดต่อเรา"
+      title="ติดต่อเรา"
+      pageType="contact"
+    />
   )
 
 }
