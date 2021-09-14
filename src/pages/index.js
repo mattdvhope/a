356a9278b0d43d20fce8a47573482ade47470527
Matt Malone <mattagape@me.com>
2021-09-14 17:36:14 +0700

    Removed PageTemplate import

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import * as React from "react"
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import PageTemplate from "../components/PageTemplate";
 
 const IndexPage = () => (
   <Layout header="Home">
