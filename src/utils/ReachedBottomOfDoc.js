3386c80ef822c2ea97883a6395a4f28247d997c2
Matt Malone <mattagape@me.com>
2021-09-14 22:12:19 +0700

    Cleaned up

--- src/utils/ReachedBottomOfDoc.js
+++ src/utils/ReachedBottomOfDoc.js
@@ -1,30 +1,11 @@
-import { detect } from "detect-browser";
 import InRangeOfDocHeight from './InRangeOfDocHeight'
 
-
 const ReachedBottomOfDoc = () => {
-
 	// // Must use Math.ceil for Android/chromium-webview to work.
   const heightAboveWindowScreen = Math.ceil(document.documentElement.scrollTop);
   const windowScreenHeight = Math.ceil(window.innerHeight);
   const heightOfDocument = Math.ceil(document.documentElement.offsetHeight);
-
-  const browser = detect();
-
-  // if (browser.name === "chromium-webview") {
-		// return InRangeOfDocHeight(heightAboveWindowScreen, windowScreenHeight, heightOfDocument); 	
-  // } else {
-  	return heightAboveWindowScreen + windowScreenHeight >= heightOfDocument;
-  // }
-
+	return heightAboveWindowScreen + windowScreenHeight >= heightOfDocument;
 }
 
-
 export default ReachedBottomOfDoc;
-
-// Various browsers:
-// browser.name === "node"
-// browser.name === "facebook"
-// browser.name === "safari"
-// browser.name === "ios"
-// browser.name === "chromium-webview"
