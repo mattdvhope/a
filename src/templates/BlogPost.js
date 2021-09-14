14629c8c123ac3c10ca09d26a9a372935d760b03
Matt Malone <mattagape@me.com>
2021-09-14 22:35:37 +0700

    Check methods

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -1,7 +1,4 @@
 import React, { useState, useRef } from "react";
-
-import { detect } from "detect-browser";
-
 import { Link, StaticQuery, graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -59,8 +56,6 @@ console.log(data.contentfulBlogs.slug);
     .catch(err => console.log("error: ", err));
   }
 
-  const browser = detect();
-
   return (
     <Layout header="feed">
       <SEO title="วีดีโอ" />
@@ -69,7 +64,6 @@ console.log(data.contentfulBlogs.slug);
         {initialVideosAbove}
         <div className="site-container blog-post" ref={firstVideoRef}>
           <YoutubeHolder data={data.contentfulBlogs}/>
-          {JSON.stringify(/node/.test(browser.name.toString()))}
           {numberOfVideosBelow === 0 ? UnderLastVideo() : null}
         </div>
         {initialVideosBelow}
