9b24bb7203934d82e78e327ca17b5bdb26f9e633
Matt Malone <mattagape@me.com>
2021-09-15 09:21:56 +0700

    Check methods

--- src/utils/SetFirstVidPosNode.js
+++ src/utils/SetFirstVidPosNode.js
@@ -9,16 +9,13 @@ const SetFirstVideoPosition = (firstVideoRef, slug) => {
 
   useEffect(() => {
 
-
     const myPromise = new Promise((resolve, reject) => {
-      // resolve(setFirstVideoPosition(firstVideoRef.current.getBoundingClientRect().top));
-      resolve();
+      resolve(setFirstVideoPosition(firstVideoRef.current.getBoundingClientRect().top));
     });
     const browser = detect();
     const scrollSpeed = browser.name === "chromium-webview" ? 1800 : 1200;
     myPromise
-    // .then(res => firstVideoPosition || firstVideoPosition === 0 ? ScrollToSmoothly(firstVideoPosition, scrollSpeed) : null)
-    .then(res => null)
+    .then(res => firstVideoPosition || firstVideoPosition === 0 ? ScrollToSmoothly(firstVideoPosition, scrollSpeed) : null)
     .catch(err => console.log("error: ", err));
 
  
