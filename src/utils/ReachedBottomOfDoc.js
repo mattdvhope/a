dba266f59e1c5a4c0e9cf387e0c886f788ac3e9b
Matt Malone <mattagape@me.com>
2021-09-15 22:29:41 +0700

    Removed browser detect

--- src/utils/ReachedBottomOfDoc.js
+++ src/utils/ReachedBottomOfDoc.js
@@ -1,20 +1,12 @@
-import { detect } from "detect-browser";
 import InRangeOfDocHeight from './InRangeOfDocHeight'
 
 const ReachedBottomOfDoc = () => {
-
 	// // Must use Math.ceil for Android/chromium-webview to work.
   const heightAboveWindowScreen = Math.ceil(document.documentElement.scrollTop);
   const windowScreenHeight = Math.ceil(window.innerHeight);
   const heightOfDocument = Math.ceil(document.documentElement.offsetHeight);
 
-  const browser = detect();
-
-  // if (browser.name === "chromium-webview") { 
-  //   return InRangeOfDocHeight(heightAboveWindowScreen, windowScreenHeight, heightOfDocument);
-  // } else {
-    return heightAboveWindowScreen + windowScreenHeight >= heightOfDocument;
-  // }
+  return heightAboveWindowScreen + windowScreenHeight >= heightOfDocument;
 }
 
 export default ReachedBottomOfDoc;
