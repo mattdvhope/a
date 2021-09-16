655a962717c02fceb6c26045736abd8f49ab899a
Matt Malone <mattagape@me.com>
2021-09-16 18:15:34 +0700

    Added about text

--- src/components/Contact2/ContactApp.jsx
+++ src/components/Contact2/ContactApp.jsx
@@ -6,7 +6,7 @@ function ContactApp({ pageType }) {
     <Contact
       pageType={pageType}
       title={pageType === "contact" ? "ติดต่อเรา" : "เกี่ยวกับเรา"}
-      text1={pageType === "contact" ? "มาแชทกับเรา! เราต้องการได้ยินจากคุณ!" : "จุดประสงค์ของเว็บไซต์นี้คืออะไร?"}
+      text1={pageType === "contact" ? "มาแชทกับเรา! เราต้องการได้ยินจากคุณ!" : "เราได้สร้างพื้นที่เว็บสำหรับผู้ที่ต้องการเรียนรู้เพิ่มเติมเกี่ยวกับพระเยซูคริสต์"}
       messengerLogo="https://anima-uploads.s3.amazonaws.com/projects/61392976e87edf415321004d/releases/613ef1fcd9c4d2c5e96f0e0f/img/messenger@2x.png"
       chat="แชท"
       facebookCircularLogo="https://anima-uploads.s3.amazonaws.com/projects/61392976e87edf415321004d/releases/614084fc8a97c95820323a78/img/facebook-circular-logo@2x.png"
