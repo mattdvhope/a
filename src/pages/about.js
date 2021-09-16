d13b856c66ea4bca03034ce2b259300c74a9965b
Matt Malone <mattagape@me.com>
2021-09-16 17:48:37 +0700

    Started setting up about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,11 @@
 import * as React from "react"
 import PageTemplate from "../components/PageTemplate"
-import Layout from "../components/layout" // REMOVE THIS SOON!!!!!!!
 
 const About = () => 
-    <Layout>;
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <h1>Hello from ABOUT!!!!!</h1>
-    </Layout>;
-
-
-
-  // <PageTemplate 
-  //   header="เกี่ยวกับเรา"
-  //   title="เกี่ยวกับเรา"
-  //   pageType="about"
-  // />
+  <PageTemplate
+    header="เกี่ยวกับเรา"
+    title="เกี่ยวกับเรา"
+    pageType="about"
+  />
 
 export default About
