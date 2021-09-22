456569997cfbcf9a898d9d5642180c641659dd36
Matt Malone <mattagape@me.com>
2021-09-22 10:49:46 +0700

    Added setTimeout for scrolling

--- src/utils/SetFirstVideoPosition.js
+++ src/utils/SetFirstVideoPosition.js
@@ -5,20 +5,30 @@ import { ScrollToSmoothly } from "./ScrollToSmoothly";
 const SetFirstVideoPosition = (firstVideoRef, slug) => { // MAYBE TEST 'async' & 'await' here......
   const [firstVidIsInPosition, setFirstVidIsInPosition] = useState(false); // this may not be necessary, but is there for good measure
   useEffect(() => {
+
     // To set default video post for the session
     window.sessionStorage.setItem('slug', slug);
 
-    // To scroll to first video's position (after initialVideosAbove & initialVideosBelow render)
-    const aPromise = new Promise((resolve, reject) => {
-      resolve();
-    });
-    aPromise
-    .then(res => {
-      if (firstVidIsInPosition) return; // this may not be necessary, but is there for good measure
-      const topOfFirstVid = firstVideoRef.current.getBoundingClientRect().top;
-      ScrollToSmoothly(topOfFirstVid, 1200);
-      setFirstVidIsInPosition(true); // this may not be necessary, but is there for good measure
-    })
+    setTimeout(function () {
+      firstVideoRef.current.scrollIntoView();
+    }, 1000);
+
+    // // To scroll to first video's position (after initialVideosAbove & initialVideosBelow render)
+    // const aPromise = new Promise((resolve, reject) => {
+    //   resolve();
+    // });
+    // aPromise
+    // .then(res => {
+    //   if (firstVidIsInPosition) return; // this may not be necessary, but is there for good measure
+    //   const topOfFirstVid = firstVideoRef.current.getBoundingClientRect().top;
+    //   ScrollToSmoothly(topOfFirstVid, 1200);
+    //   setFirstVidIsInPosition(true); // this may not be necessary, but is there for good measure
+    // })
+
+
+
+
+
   }, []); // useEffect
 };
 
