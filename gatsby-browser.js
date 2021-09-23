29b7be87db7c0ed6bfde2a5284e96093e6f7ddd0
Matt Malone <mattagape@me.com>
2021-09-23 15:46:30 +0700

    Refactored Messenger Chat

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,14 @@
+import React from "react";
+import Layout from "./src/components/layout";
+import MessengerChat from "./src/components/MessengerChat";
+
+export const wrapPageElement = ({ element }) => (
+  <>
+    {element}
+    <MessengerChat />
+  </>
+);
+
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,34 +1,56 @@
-import React from "react"
-// This is for the website FB Chat Messenger (on bottom-right of website)
-export const onRenderBody = (
-  { setPreBodyComponents },
-  pluginOptions
-) => {
-  setPreBodyComponents([
-    <div id="fb-root"></div>,
-    <div className="fb-customerchat"
-      page_id="106619367753772"
-      attribution="biz_inbox">
-    </div>,
-    <script
-      key="fun_javascript"
-      dangerouslySetInnerHTML={{ __html: `
-        window.fbAsyncInit = function() {
-          window.FB.init({
-            xfbml            : ${true},
-            version          : 'v11.0'
-          });
-        };
+import React from "react";
+import Layout from "./src/components/layout";
+import MessengerChat from "./src/components/MessengerChat";
 
-        (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-      `,
-      }}
-    />
-  ])
-}
+export const wrapPageElement = ({ element }) => (
+  <>
+    {element}
+    <MessengerChat />
+  </>
+);
+
+
+// export const wrapPageElement = ({ element, props }) => {
+//     return <Layout {...props}>
+//                {element}
+//                <MessengerChat />
+//            </Layout>
+  
+//   }
+
+
+
+// import React from "react"
+// // This is for the website FB Chat Messenger (on bottom-right of website)
+// export const onRenderBody = (
+//   { setPreBodyComponents },
+//   pluginOptions
+// ) => {
+//   setPreBodyComponents([
+//     <div id="fb-root"></div>,
+//     <div className="fb-customerchat"
+//       page_id="106619367753772"
+//       attribution="biz_inbox">
+//     </div>,
+//     <script
+//       key="fun_javascript"
+//       dangerouslySetInnerHTML={{ __html: `
+//         window.fbAsyncInit = function() {
+//           window.FB.init({
+//             xfbml            : ${true},
+//             version          : 'v11.0'
+//           });
+//         };
+
+//         (function(d, s, id) {
+//           var js, fjs = d.getElementsByTagName(s)[0];
+//           if (d.getElementById(id)) return;
+//           js = d.createElement(s); js.id = id;
+//           js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+//           fjs.parentNode.insertBefore(js, fjs);
+//         }(document, 'script', 'facebook-jssdk'));
+//       `,
+//       }}
+//     />
+//   ])
+// }
