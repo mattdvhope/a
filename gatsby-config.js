ebffe1bc65a5418f42989ac689d95c30551b4c3c
Matt Malone <mattagape@me.com>
2021-09-30 09:51:55 +0700

    Set up new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,15 +70,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Matt Malone`,
+        short_name: `Matt`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-birds.png`, // This is the favicon.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
