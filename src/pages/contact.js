4a7cf98f07b88967ce52d9d0eaba9a59985c47f1
Matt Malone <mattagape@me.com>
2021-10-05 06:55:03 +0700

    Added post request

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,40 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react";
 import PageTemplate from "../components/PageTemplate"
 
-const Contact = () => 
-  <PageTemplate
-    header="ติดต่อเรา"
-    title="ติดต่อเรา"
-    pageType="contact"
-  />
+
+const Contact = () => {
+
+  useEffect(() => { // This for Luke's backend!!!!!
+
+    let _data = {
+      userid: "user-7j2kgE5H8LGYpRdT8KmGbtUSC"
+    }
+
+    const request = new Request(
+      'https://e2doix.herokuapp.com/feed' //,
+      // {headers: {'X-CSRFToken': csrftoken}}
+    );
+    fetch(request, {
+      method: 'POST',
+      body: JSON.stringify(_data)
+    }).then(function(response) {
+      console.log(response);
+    }).catch(function(error) {
+      console.log(error);
+    });
+
+  }, []);
+
+
+  return (
+    <PageTemplate
+      header="ติดต่อเรา"
+      title="ติดต่อเรา"
+      pageType="contact"
+    />
+  )
+
+}
+
 
 export default Contact
